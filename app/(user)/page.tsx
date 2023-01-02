2cb1f75f42d78dfbbd67092cea96cc1b77c18050
acmhub <112704585+acmhub@users.noreply.github.com>
2023-01-02 17:04:46 +0200

    implement sanity studio preview mode

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,3 +1,37 @@
-export default function HomePage() {
-	return <div></div>;
+import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "../../lib/sanity.client";
+import PreviewSuspense from "../../components/PreviewSuspense";
+import PreviewBlogList from "../../components/PreviewBlogList";
+import BlogList from "../../components/BlogList";
+
+export const query = groq`
+	*[_type=='post'] {
+		...,
+		author->,
+		categories[]->
+	} | order(_createdAt desc)
+`;
+
+export default async function HomePage() {
+	if (previewData()) {
+		return (
+			<PreviewSuspense
+				fallback={
+					<div role="status">
+						<p className="text-center text-lg">
+							loading preview data...
+						</p>
+					</div>
+				}
+			>
+				<div>Preview mode</div>
+				<PreviewBlogList query={query} />
+			</PreviewSuspense>
+		);
+	}
+
+	const posts = await client.fetch(query);
+
+	return <BlogList posts={posts} />;
 }
